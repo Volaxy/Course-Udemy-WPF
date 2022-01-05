ba6f75d7277eab40e5391664b0a2f3157800705f
Gustavo Martins Pereira <guga.pro.00@hotmail.com>
2022-01-05 16:22:10 -0300

    ADD - Chapter 7 Completed 1/2

---
+++ 07 - Grid and Common Controls/01 - Add Grid to Window and use it_Sn/01 - Add Grid to Window and use it/App.xaml.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace _01___Add_Grid_to_Window_and_use_it
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+}
---
+++ 07 - Grid and Common Controls/04 - Label Control and Properties_Sn/04 - Label Control and Properties/App.xaml.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace _04___Label_Control_and_Properties
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+}

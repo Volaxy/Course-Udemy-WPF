2f0c56cba36a21e6a8e27819f08f3b2aa339cff0
Gustavo Martins Pereira <guga.pro.00@hotmail.com>
2022-01-06 15:24:28 -0300

    ADD - Chapter 11 Completed

---
+++ 11 - Using Margin and Alignment/03 - Auto Move Controls by Margin_Sn/03 - Auto Move Controls by Margin/App.xaml.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace _03___Auto_Move_Controls_by_Margin
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+}

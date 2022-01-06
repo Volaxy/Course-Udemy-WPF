d4cffe891f7cd720ba55fcb530cf99581d8e8d4b
Gustavo Martins Pereira <guga.pro.00@hotmail.com>
2022-01-06 14:25:54 -0300

    ADD - Chapter 10 Completed

---
+++ 10 - Advance WPF App Design/02 - Grouping Controls Together_Sn/02 - Grouping Controls Together/App_GroupingControls.xaml.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace _02___Grouping_Controls_Together
+{
+    /// <summary>
+    /// Interaction logic for App_GroupingControls.xaml
+    /// </summary>
+    public partial class App_GroupingControls : Window
+    {
+        public App_GroupingControls()
+        {
+            InitializeComponent();
+        }
+    }
+}

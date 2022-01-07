36069a64ec6b67ba773e4cfd003a4e660ef6ee26
Gustavo Martins Pereira <guga.pro.00@hotmail.com>
2022-01-07 15:19:30 -0300

    ADD - Chapter 13 Completed

---
+++ 14 - Start WPF Coding with C#/01 - Add Folder to Project to Group_Sn/01 - Add Folder to Project to Group/App.xaml.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace _01___Add_Folder_to_Project_to_Group
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+}

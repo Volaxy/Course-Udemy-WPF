cec81fbae0ad6180773906a1ea29f69ba2a0c92f
Gustavo Martins Pereira <guga.pro.00@hotmail.com>
2022-01-11 11:34:04 -0300

    ADD - Chapter 19 Completed

---
+++ 19 - Using Menu Control/03 - Add Sub Menus to Main Menu_Sn/03 - Add Sub Menus to Main Menu/App.xaml.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace _03___Add_Sub_Menus_to_Main_Menu
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+}

4dc17d1addf08225f5b66a9ea3438ba0d197d917
Gustavo Martins Pereira <guga.pro.00@hotmail.com>
2022-01-12 12:44:08 -0300

    ADD - Chapter 22 - Version Information

--- 22 - Notepad App/Notepad_Sn/Notepad/Properties/AssemblyInfo.cs
+++ 22 - Notepad App/Notepad_Sn/Notepad/Properties/AssemblyInfo.cs
@@ -8,12 +8,12 @@ using System.Windows;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("Notepad")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyDescription("First Notepad App by WPF")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("Notepad")]
+[assembly: AssemblyCompany("Etaure")]
+[assembly: AssemblyProduct("WPF Notepad")]
 [assembly: AssemblyCopyright("Copyright ©  2022")]
-[assembly: AssemblyTrademark("")]
+[assembly: AssemblyTrademark("VLX")]
 [assembly: AssemblyCulture("")]
 
 // Setting ComVisible to false makes the types in this assembly not visible
@@ -51,5 +51,6 @@ using System.Windows;
 // You can specify all the values or you can default the Build and Revision Numbers
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyVersion("2022.01.12.1")]
+[assembly: AssemblyFileVersion("2022.01.12.1")]
+[assembly: NeutralResourcesLanguage("en-US")]

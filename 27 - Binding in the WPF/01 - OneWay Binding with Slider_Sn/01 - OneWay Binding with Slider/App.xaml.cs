72ff0098df7dd667c5664559a00237f30268fe57
Gustavo Martins Pereira <guga.pro.00@hotmail.com>
2022-01-14 13:44:01 -0300

    ADD - Chapter 27 Completed

---
+++ 27 - Binding in the WPF/01 - OneWay Binding with Slider_Sn/01 - OneWay Binding with Slider/App.xaml.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace _01___OneWay_Binding_with_Slider
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+}

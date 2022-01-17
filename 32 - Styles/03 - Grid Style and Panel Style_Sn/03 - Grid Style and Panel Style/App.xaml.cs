a817e212460bebb370276d1b306a4dd2f886d9bd
Gustavo Martins Pereira <guga.pro.00@hotmail.com>
2022-01-17 15:19:11 -0300

    ADD - Chapter 32 Completed

---
+++ 32 - Styles/03 - Grid Style and Panel Style_Sn/03 - Grid Style and Panel Style/App.xaml.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace _03___Grid_Style_and_Panel_Style
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+}

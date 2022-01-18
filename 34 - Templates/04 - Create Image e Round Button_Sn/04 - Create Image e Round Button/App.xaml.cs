6321a7e24364ad541c79199fd81079a195ebcec7
Gustavo Martins Pereira <guga.pro.00@hotmail.com>
2022-01-18 13:22:39 -0300

    ADD - Chapter 34 Completed

---
+++ 34 - Templates/04 - Create Image e Round Button_Sn/04 - Create Image e Round Button/App.xaml.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace _04___Create_Image_e_Round_Button
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+}

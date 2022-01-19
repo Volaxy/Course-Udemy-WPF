e90e9eba4f2c17db5e72db8160394b6e27271864
Gustavo Martins Pereira <guga.pro.00@hotmail.com>
2022-01-19 15:28:25 -0300

    ADD - Chapter 37 Completed 1/2

---
+++ 37 - ListView/03 - Add Items to ListView at Once_Sn/03 - Add Items to ListView at Once/App.xaml.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace _03___Add_Items_to_ListView_at_Once
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+}
---
+++ 37 - ListView/03 - Add Items to ListView at Once_Sn/03 - Add Items to ListView at Once/MainWindow.xaml.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace _03___Add_Items_to_ListView_at_Once
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void addButton_Click(object sender, RoutedEventArgs e)
+        {
+            //this.mainListView.Items.Add("Edit");
+            //this.mainListView.Items.Add("New");
+            //this.mainListView.Items.Add("Delete");
+
+            this.mainListView.Items.Add(this.nameTextBox.Text);
+            this.countLabel.Content = this.mainListView.Items.Count.ToString();
+        }
+
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+            this.countLabel.Content = this.mainListView.Items.Count.ToString();
+        }
+
+        private void deleteAllButton_Click(object sender, RoutedEventArgs e)
+        {
+            this.mainListView.Items.Clear();
+        }
+
+        private void deleteSelectedButton_Click(object sender, RoutedEventArgs e)
+        {
+            //int index = this.mainListView.SelectedIndex;
+
+            //this.mainListView.Items.RemoveAt(index);
+            //******************************************
+            object item = this.mainListView.SelectedItem;
+
+            this.mainListView.Items.Remove(item);
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            List<string> list = new List<string>();
+
+            list.Add("Male");
+            list.Add("Female");
+            list.Add("Boy");
+            list.Add("Girl");
+
+            this.secondListView.ItemsSource = list;
+        }
+    }
+}

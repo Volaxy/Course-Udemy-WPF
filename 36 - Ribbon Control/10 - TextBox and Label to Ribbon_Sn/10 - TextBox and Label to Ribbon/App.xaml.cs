80f3422769a18d65a254683ae6586341ef3779aa
Gustavo Martins Pereira <guga.pro.00@hotmail.com>
2022-01-19 14:37:48 -0300

    ADD - Chapter 36 Completed

---
+++ 36 - Ribbon Control/10 - TextBox and Label to Ribbon_Sn/10 - TextBox and Label to Ribbon/App.xaml.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace _10___TextBox_and_Label_to_Ribbon
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+}
---
+++ 36 - Ribbon Control/10 - TextBox and Label to Ribbon_Sn/10 - TextBox and Label to Ribbon/MainWindow.xaml.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace _10___TextBox_and_Label_to_Ribbon
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void cutRibbonButton_Click(object sender, RoutedEventArgs e)
+        {
+            System.Windows.MessageBox.Show("I'm cut button!");
+        }
+
+        private void copyRibbonButton_Click(object sender, RoutedEventArgs e)
+        {
+            System.Windows.MessageBox.Show("I'm copy button!");
+        }
+
+        private void pasteRibbonMenuItem_Click(object sender, RoutedEventArgs e)
+        {
+            System.Windows.MessageBox.Show("I'm paste button!");
+        }
+
+        private void pasteSpecialRibbonMenuItem_Click(object sender, RoutedEventArgs e)
+        {
+            System.Windows.MessageBox.Show("I'm special paste button!");
+        }
+
+        private void pasteSpecialRibbonMenuItem_Checked(object sender, RoutedEventArgs e)
+        {
+            System.Windows.Controls.Ribbon.RibbonMenuItem ribbonMenuItem;
+            ribbonMenuItem = (System.Windows.Controls.Ribbon.RibbonMenuItem)sender;
+
+            if (ribbonMenuItem.IsChecked)
+            {
+                System.Windows.MessageBox.Show("Paste button is checked!");
+            }
+        }
+
+        private void exitRibbonMenuItem_Click(object sender, RoutedEventArgs e)
+        {
+            this.Close();
+        }
+    }
+}
